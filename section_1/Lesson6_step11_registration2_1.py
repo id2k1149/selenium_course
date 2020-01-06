6b428baf570519af6dc9a1b1228c64f7e2f92f93
MikePolyakov <id2k1149@gmail.com>
2020-01-06 05:25:09 +0300

    week 1

---
+++ section_1/Lesson6_step11_registration2_1.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[1]/input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[2]/input")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[3]/input")
+    input3.send_keys("123")
+
+    button = browser.find_element_by_class_name("btn.btn-default")
+    button.click()
+    # Ваш код, который заполняет обязательные поля
+
+    # Отправляем заполненную форму
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+    link = "http://suninjuly.github.io/registration2.html"
+    browser.get(link)
+
+    input1 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[1]/input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[2]/input")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[3]/input")
+    input3.send_keys("123")
+
+    button = browser.find_element_by_class_name("btn.btn-default")
+    button.click()
+
+# Отправляем заполненную форму
+
+# Проверяем, что смогли зарегистрироваться
+# ждем загрузки страницы
+    time.sleep(1)
+
+# находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+# записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
