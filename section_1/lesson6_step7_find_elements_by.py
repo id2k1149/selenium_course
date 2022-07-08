728e8dac06546f9bc76cec26428c179b1a984774
id2k1149 <id2k1149@gmail.com>
2022-07-08 11:01:59 -0400

    find elements

--- section_1/lesson6_step7_find_elements_by.py
+++ section_1/lesson6_step7_find_elements_by.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 browser = webdriver.Chrome()
 
 try:
     browser.get("http://suninjuly.github.io/huge_form.html")
-    elements = browser.find_elements_by_tag_name("input")
+    # elements = browser.find_elements_by_tag_name("input")
+    elements = browser.find_elements(By.TAG_NAME, "input")
 
     for element in elements:
         element.send_keys("Мой ответ")
 
-    button = browser.find_element_by_css_selector("button.btn")
+    # button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
 finally:
