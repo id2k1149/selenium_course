a477b5184d239f6ac785c766aa95e30c843654db
id2k1149 <id2k1149@gmail.com>
2022-07-08 09:45:18 -0400

    lesson4

--- section_1/lesson4_step3_find_tag.py
+++ section_1/lesson4_step3_find_tag.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
 
-print(len(BeautifulSoup
-          (requests.get('http://suninjuly.github.io/blog_example.html').
-           text, 'html.parser').find_all('div')))
+print(len(BeautifulSoup(requests.get('http://suninjuly.github.io/blog_example.html').text, 'html.parser')
+          .find_all('div')))
